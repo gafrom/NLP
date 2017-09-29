837ecf5c8e113f332c9fcbb7b047b282da7e8431
Oleg <gafrom@gmail.com>
2017-09-29 14:37:19 +0300

    Refactor test worker to print out test data

--- worker.py
+++ worker.py
@@ -1,8 +1,12 @@
 from nlp.preparer import Corpus
 
-corpus = Corpus('./articles/good.articles')
+corpora_paths = ['./articles/good.articles', './articles/bad.articles'] 
 
-print(f"Corpus label: {corpus.label}")
-print(f"Corpus article: {corpus.raw[0]}")
-print(f"Corpus data: {corpus.articles[0]}")
-print(f"Corpus av length: {corpus.average_length()}")
+for path in corpora_paths:
+  corpus = Corpus(path)
+
+  print(f"Corpus label: {corpus.label}")
+  print(f"Corpus article: {corpus.raw[0]}")
+  print(f"Corpus length: {len(corpus.articles)}")
+  print(f"Corpus data: {corpus.articles[0]}")
+  print(f"Corpus av length: {corpus.average_length()}")
