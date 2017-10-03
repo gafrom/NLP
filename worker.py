78fdf38377729966a012f05dadd2b905e142f299
Oleg <gafrom@gmail.com>
2017-10-03 13:03:30 +0300

    Update print wording

--- worker.py
+++ worker.py
@@ -2,11 +2,18 @@ from nlp.preparer import Corpus
 
 corpora_paths = ['./articles/good.articles', './articles/bad.articles'] 
 
+corpora = []
+lengths = []
+
 for path in corpora_paths:
   corpus = Corpus(path)
+  corpora.append(corpus)
+  length = [len(article) for article in corpus.articles]
+  lengths.append(length)
 
-  print(f"Corpus label: {corpus.label}")
-  print(f"Corpus article: {corpus.raw[0]}")
-  print(f"Corpus length: {len(corpus.articles)}")
-  print(f"Corpus data: {corpus.articles[0]}")
-  print(f"Corpus av length: {corpus.average_length()}")
+  print(f"Corpus label: {corpus.label}, ",
+        f"length: {len(corpus.articles)} articles, ",
+        f"av length: {round(corpus.average_length())} words, ",
+        f"max length: {max(length)} words.")
+  print(f"Corpus raw article: {corpus.raw[0][:200]}")
+  print(f"Corpus data (words): {corpus.articles[0][:20]}\n")
