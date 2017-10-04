ed9cb885b55284becb2227254cf2d58bdbf0fa02
Oleg <gafrom@gmail.com>
2017-10-04 15:55:28 +0300

    Implement state modifiers in BatchIterator to allow resetting lstm state across articles

--- nlp/iterator.py
+++ nlp/iterator.py
@@ -2,14 +2,14 @@
 # =============
 #
 # batch_size = 3
-# a = [['A', 'B', 'C', 'D'], ['E', 'F'], ['G'], ['H', 'I', 'J']]
-# b = [['a', 'b', 'c'], ['d'], ['e', 'f']]
-#
+# a = [['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H'], ['I'], ['J', 'K', 'L']]
+# b = [['a', 'b', 'c'], ['d', 'e'], ['f', 'g', 'h']]
+
 # print(f'Good articles({len(a)}): {a}')
 # print(f'Bad  articles({len(b)}): {b}')
-#
+
 # batches = BatchIterator(batch_size, a, b, 0, 1)
-#
+
 # print(f'\nGenerated batches of length {batch_size}:')
 # for batch in batches:
 #   print(batch)
@@ -18,15 +18,16 @@
 # Output
 # ======
 #
-# Good articles(4): [['A', 'B', 'C', 'D'], ['E', 'F'], ['G'], ['H', 'I', 'J']]
-# Bad  articles(3): [['a', 'b', 'c'], ['d'], ['e', 'f']]
-#
+# Good articles(4): [['A', 'B', 'C', 'D'], ['E', 'F', 'G', 'H'], ['I'], ['J', 'K', 'L']]
+# Bad  articles(3): [['a', 'b', 'c'], ['d', 'e'], ['f', 'g', 'h']]
+
 # Generated batches of length 3:
-# [['A', 'B', 'C'], [0, 0, 0]]
-# [['D', 'a', 'b'], [0, 1, 1]]
-# [['c', 'E', 'F'], [1, 0, 0]]
-# [['d', 'G', 'e'], [1, 0, 1]]
-# [['f'], [1]]
+# [['A', 'B', 'C'], [0, 0, 0], [1.0, 1.0, 1.0]]
+# [['D', 'a', 'b'], [0, 1, 1], [1.0, 0.0, 1.0]]
+# [['c', 'E', 'F'], [1, 0, 0], [1.0, 0.0, 1.0]]
+# [['G', 'H', 'd'], [0, 0, 1], [1.0, 1.0, 0.0]]
+# [['e', 'I', 'f'], [1, 0, 1], [1.0, 0.0, 0.0]]
+# [['g', 'h'], [1, 1], [1.0, 1.0]]
 #
 class BatchIterator(object):
   def __init__(self, size, a, b, vec_a, vec_b):
@@ -34,6 +35,7 @@ class BatchIterator(object):
     self.current = 0
     self.x = [item for x, y in zip(a, b) for item in x + y]
     self.y = [item for x, y in zip(a, b) for item in [vec_a for _ in x] + [vec_b for _ in y]]
+    self.z = self._state_cleaners()
     self.length = len(self.x)
 
   def __iter__(self):
@@ -46,7 +48,8 @@ class BatchIterator(object):
       current_index = self.current * self.size
       self.current += 1
       return [self.x[current_index:current_index + self.size],
-              self.y[current_index:current_index + self.size]]
+              self.y[current_index:current_index + self.size],
+              self.z[current_index:current_index + self.size]]
 
   # this method rewinds the iterator to make an infinite loop
   def next(self):
@@ -57,3 +60,18 @@ class BatchIterator(object):
       result = self.__next__()
 
     return result
+
+  def _state_cleaners(self):
+    modifiers = []
+    previous_label = self.y[0]
+
+    for label in self.y:
+      if label == previous_label:
+        modifier = 1.
+      else:
+        modifier = 0.
+
+      previous_label = label
+      modifiers.append(modifier)
+
+    return modifiers
