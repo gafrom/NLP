c8333b4d6abb69c5615aa3c4b9904e7c818a496e
Oleg <gafrom@gmail.com>
2017-10-30 15:04:59 +0300

    Use graph meta data to import graph

--- nlp/predictor.py
+++ nlp/predictor.py
@@ -3,65 +3,6 @@ from pathlib import Path
 import pickle
 from nlp.cleaner import Cleaner
 
-class Model(object):
-  def __init__(self, embedding_size, num_nodes, vocabulary_size, num_classes):
-    self.graph = tf.Graph()
-    with self.graph.as_default():
-      # Parameters:
-      # Input gate: input, previous output, and bias.
-      ix = tf.Variable(tf.truncated_normal([embedding_size, num_nodes], -0.1, 0.1), name='ix')
-      im = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1), name='im')
-      ib = tf.Variable(tf.zeros([1, num_nodes]), name='ib')
-      # Forget gate: input, previous output, and bias.
-      fx = tf.Variable(tf.truncated_normal([embedding_size, num_nodes], -0.1, 0.1), name='fx')
-      fm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1), name='fm')
-      fb = tf.Variable(tf.zeros([1, num_nodes]), name='fb')
-      # Output gate: input, previous output, and bias.
-      ox = tf.Variable(tf.truncated_normal([embedding_size, num_nodes], -0.1, 0.1), name='ox')
-      om = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1), name='om')
-      ob = tf.Variable(tf.zeros([1, num_nodes]), name='ob')
-      # Memory cell: input, state and bias.                             
-      cx = tf.Variable(tf.truncated_normal([embedding_size, num_nodes], -0.1, 0.1), name='cx')
-      cm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1), name='cm')
-      cb = tf.Variable(tf.zeros([1, num_nodes]), name='cb')
-      # Variables saving state across unrollings.
-      saved_output = tf.Variable(tf.zeros([1, num_nodes]), trainable=False)
-      saved_state = tf.Variable(tf.zeros([1, num_nodes]), trainable=False)
-      # Classifier weights and biases.
-      w = tf.Variable(tf.truncated_normal([num_nodes, num_classes], -0.1, 0.1), name='w')
-      b = tf.Variable(tf.zeros([num_classes]), name='b')
-      # Embeddings
-      embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0), name='embeddings')
-
-      # Definition of the cell computation.
-      def lstm_cell(i, o, state):
-        # Create an LSTM cell
-        input_gate =  tf.sigmoid(tf.matmul(i, ix) + tf.matmul(o, im) + ib)
-        forget_gate = tf.sigmoid(tf.matmul(i, fx) + tf.matmul(o, fm) + fb)
-        output_gate = tf.sigmoid(tf.matmul(i, ox) + tf.matmul(o, om) + ob)
-        update = tf.matmul(i, cx) + tf.matmul(o, cm) + cb
-        state = forget_gate * state + input_gate * tf.tanh(update)
-
-        return output_gate * tf.tanh(state), state
-
-      # Sampling and validation eval: batch 1, no unrolling.
-      self.sample_input = tf.placeholder(tf.int32, shape=[1])
-      sample_lstm_input = tf.nn.embedding_lookup(embeddings, self.sample_input)
-      saved_sample_output = tf.Variable(tf.zeros([1, num_nodes]))
-      saved_sample_state = tf.Variable(tf.zeros([1, num_nodes]))
-      self.reset_sample_state = tf.group(
-        saved_sample_output.assign(tf.zeros([1, num_nodes])),
-        saved_sample_state.assign(tf.zeros([1, num_nodes])))
-      sample_output, sample_state = lstm_cell(
-        sample_lstm_input, saved_sample_output, saved_sample_state)
-      with tf.control_dependencies([saved_sample_output.assign(sample_output),
-                                    saved_sample_state.assign(sample_state)]):
-        self.sample_prediction = tf.nn.softmax(tf.nn.xw_plus_b(sample_output, w, b))
-
-      variables_to_restore = [ix, im, ib, fx, fm, fb, ox, om, ob, cx, cm, cb, w, b, embeddings]
-      self.saver = tf.train.Saver(variables_to_restore)
-
-
 class Dictionary(object):
   def __init__(self, path):
     self.path = path
@@ -79,18 +20,20 @@ class Predictor(object):
   STORAGE_PATH = './storage/model'
 
   def __init__(self):
-    print('Loading neural network Graph...')
-    self.model = Model(128, 64, 25000, 2)
-    self.saver              = self.model.saver
-    self.graph              = self.model.graph
-    self.sample_input       = self.model.sample_input
-    self.reset_sample_state = self.model.reset_sample_state
-    self.sample_prediction  = self.model.sample_prediction
-
-    self.session = tf.Session(graph=self.graph)
-    self.saver.restore(self.session, self.STORAGE_PATH)
+    print('Loading neural network Graph...', end='')
+    self.session = tf.Session()
+    saver = tf.train.import_meta_graph(f"{self.STORAGE_PATH}.meta")
     print('Done')
-    print('Loading dictionary...')
+
+    print('Loading neural network brains...', end='')
+    saver.restore(self.session, self.STORAGE_PATH)
+    self.graph = tf.get_default_graph()
+    self.sample_input       = self.graph.get_tensor_by_name('sample_input:0')
+    self.reset_sample_state = self.graph.get_operation_by_name('reset_sample_state')
+    self.sample_prediction  = self.graph.get_tensor_by_name('sample_prediction:0')
+    print('Done')
+
+    print('Loading dictionary...', end='')
     self.dictionary, self.reverse_dictionary, _ = Dictionary(self.STORAGE_PATH).load()
     print('Done')
     self.cleaner = Cleaner()
