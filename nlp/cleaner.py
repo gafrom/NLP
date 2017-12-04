c03b6c871f5af869183a398f19027914647ca4a6
Oleg <gafrom@gmail.com>
2017-12-04 16:20:11 +0300

    Add non-stemming option to Cleaner

--- nlp/cleaner.py
+++ nlp/cleaner.py
@@ -4,16 +4,21 @@ import re
 class Cleaner(object):
   stemmer = Stemmer.Stemmer('russian')
 
-  def __init__(self, locale='ru'):
+  def __init__(self, stemming=True, locale='ru'):
     assert locale == 'ru'
+    self._stemming = stemming
 
   def words(self, text):
     cleaned_words = self._clean(text).split(' ')
-    stemmed_words = self._stem(cleaned_words)
+    if cleaned_words[0]  == '': cleaned_words = cleaned_words[1:]
+    if len(cleaned_words) > 0 and cleaned_words[-1] == '': cleaned_words = cleaned_words[:-1]
 
-    return stemmed_words
+    words = self._stem(cleaned_words) if self._stemming is True else cleaned_words
+
+    return words
 
   def _clean(self, string):
+    string = re.sub(r"[ёЁ]",               "е",     string)
     string = re.sub(r"[^А-Яа-я0-9(),!?.]", " ",     string)
     string = re.sub(r"\d+(\.|,)?\d*",      " num ", string)
     string = re.sub(r"\.",                 " . ",   string)
