05acb17863367ecbfb017f3a6bbbb6e7f4f19d14
Oleg <gafrom@gmail.com>
2017-12-05 21:49:57 +0300

    Refactor Predictor to smoothly handle unique words

--- nlp/predictor.py
+++ nlp/predictor.py
@@ -1,42 +1,65 @@
 import tensorflow as tf
-from nlp.dictionary import Dictionary
+import numpy as np # just to print out predictions
+
 from nlp.cleaner import Cleaner
+from nlp.dictionary import Dictionary
 
 class Predictor(object):
-  STORAGE_PATH = './storage/model'
+  STORAGE_PATH = 'storage/model'
 
   def __init__(self):
-    print('🚜 Loading neural network Graph... ', end='')
     self.session = tf.Session()
-    saver = tf.train.import_meta_graph(f"{self.STORAGE_PATH}.meta")
-    print('Done ✅')
-
-    print('🚜 Loading neural network brains...', end='')
-    saver.restore(self.session, self.STORAGE_PATH)
-    self.graph = tf.get_default_graph()
-    self.sample_input       = self.graph.get_tensor_by_name('sample_input:0')
-    self.reset_sample_state = self.graph.get_operation_by_name('reset_sample_state')
-    self.sample_prediction  = self.graph.get_tensor_by_name('sample_prediction:0')
-    print('Done ✅')
-
-    print('🚜 Loading dictionary...', end='')
-    self.dictionary, self.reverse_dictionary, _ = Dictionary(self.STORAGE_PATH).load()
-    print('Done ✅')
+    self._import_graph()
+    self.sample_input, self.reset_sample_state, self.sample_prediction = self._load_brains()
+    self._load_dictionary()
     self.cleaner = Cleaner()
+
+    print('Predictor was successfully initialized ✅')
 
   def classify(self, text):
     words = self.cleaner.words(text)
+    if len(words) == 0: return None
+
+    self._replace_unique(words)
 
     self.session.run(self.reset_sample_state)
-    predicted = []
+    predicted = ''
     for word in words:
       prediction = self.session.run(self.sample_prediction, { self.sample_input: [self.dictionary[word]] })
-      predicted.append(prediction[0])
+      final = prediction[0][0]
+      predicted += np.array2string(np.round(prediction[0], 3), separator=',', precision=3)
+      predicted += ','
 
-    final = predicted[-1][0]
     if final > 0.6:   klass = 'Good'
     elif final > 0.4: klass = 'Neutral'
     else:             klass = 'Bad'
 
-    msg = f"{text}\n{words}\n{predicted}"
+    msg = f"{text}\n{words}\n{predicted[:-1]}"
     return f"Your article is {klass}:\n{msg}"
+
+  def _import_graph(self):
+    print('Loading neural network Graph...', end='')
+    self.saver = tf.train.import_meta_graph(f"{self.STORAGE_PATH}.meta")
+    print('Done ✅')
+
+  def _load_brains(self):
+    print('Loading neural network brains...', end='')
+    self.saver.restore(self.session, self.STORAGE_PATH)
+    graph = tf.get_default_graph()
+    brains = (graph.get_tensor_by_name('sample_input:0'),
+              graph.get_operation_by_name('reset_sample_state'),
+              graph.get_tensor_by_name('sample_prediction:0'))
+    print('Done ✅')
+
+    return brains
+
+  def _load_dictionary(self):
+    print('Loading dictionary...', end='')
+    self.dictionary, self.reverse_dictionary, _ = Dictionary(self.STORAGE_PATH).load()
+    print('Done ✅')
+
+  def _replace_unique(self, words):
+    print('Loading dictionary...', end='')
+    for i, word in enumerate(words):
+      if word not in self.dictionary: words[i] = Dictionary.UNK
+    print('Done ✅')
