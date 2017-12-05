48d04235ee31367f252e3d2ae75dfae8e818e197
Oleg <gafrom@gmail.com>
2017-12-05 13:01:39 +0300

    Implement truncating and stemming options in Corpus preparer

--- nlp/preparer.py
+++ nlp/preparer.py
@@ -2,13 +2,14 @@ from nlp.cleaner import Cleaner
 import re
 
 class Corpus(object):
-  def __init__(self, filename, max_words_per_article=None, min_words_per_article=4):
+  def __init__(self, filename, max_words_per_article=None, min_words_per_article=4,
+                     truncating=True, stemming=True):
     self._filename   = filename
     self._max_length = max_words_per_article
     self._min_length = min_words_per_article
-    self._bottom     = self._max_length / 4
+    self._truncating = truncating
     self.length      = None
-    self.cleaner     = Cleaner()
+    self.cleaner     = Cleaner(stemming)
 
     self.label    = self._set_label()
     self.raw      = self._load()
@@ -39,23 +40,38 @@ class Corpus(object):
     return articles
 
   def _articles(self):
+    fit_by_limit = self._truncate_by_limit if self._truncating is True else self._split_by_limit
+
     data = []
     for article in self.raw:
       words = self.cleaner.words(article)
-      data += self._split_by_limit(words)
+      data += fit_by_limit(words)
 
     return data
+
+  def _truncate_by_limit(self, collection):
+    if len(collection) < self._min_length: return []
+    if self._max_length is None: return [collection]
+
+    temp   = []
+    for i, item in enumerate(collection):
+      temp.append(item)
+      if item == '.' and i > self._max_length: break
+
+    return [temp]
 
   def _split_by_limit(self, collection):
     if len(collection) < self._min_length: return []
     if self._max_length is None: return [collection]
+
+    bottom = self._max_length / 4
 
     result = []
     temp   = []
     for i, item in enumerate(collection):
       temp.append(item)
       if item == '.' and i > (len(result) + 1) * self._max_length:
-        if len(collection) - i < self._bottom:
+        if len(collection) - i < bottom:
           temp += collection[i + 1:]
           break
         result.append(temp)
