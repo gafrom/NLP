ca3af94a40e17ebb864709904b40401affecc47c
Oleg <gafrom@gmail.com>
2018-08-23 23:04:56 +0300

    Add Laplace smoothing and calculate probabilities in log space

--- nlp/nb.py
+++ nlp/nb.py
@@ -1,6 +1,7 @@
 import collections
 from nlp.cleaner import Cleaner
 import numpy as np
+import math
 
 class NBClassifier(object):
   VOCABULARY_SIZE = 50000
@@ -22,7 +23,9 @@ class NBClassifier(object):
 
     for _, documents in labeled_documents.groupby('label'):
       count = self.count_words(self.extract_words(documents))
-      term_freq_by_class.append(self.build_term_frequencies(count))
+      term_freq = self.build_term_frequencies(count)
+      self.prepare_for_laplace_smoothing(term_freq)
+      term_freq_by_class.append(term_freq)
 
     self.term_freq_by_class = np.array(term_freq_by_class)
     self.prob_Ck = 1 / len(self.term_freq_by_class)
@@ -33,26 +36,39 @@ class NBClassifier(object):
     tokens = self.tokenize(text)
     return self.infer_from(tokens)
 
-# private
+# private --------------------------------------------------------------------------
 
   def infer_from(self, tokens):
-    prob_Ck_given_x = (self.prob_Ck / self.prob_x(tokens)) * self.prob_x_given_Ck(tokens)
+    prob_Ck_given_x = self.prob_Ck * self.likelihood(tokens) / self.evidence(tokens)
     return self.result_class(np.argmax(prob_Ck_given_x), prob_Ck_given_x)
 
-  def prob_x(self, x):
-    freqs = np.vectorize(self.term_freq.get)(x) / self.VOCABULARY_SIZE
-    return np.prod(freqs)
-
-  def prob_x_given_Ck(self, x):
-    # TODO: account for unknown words
-    # known_tokens = [token for token in x if token in self.term]
-
-    fr_by_cl = []
+  # prob_x_given_Ck
+  def likelihood(self, tokens):
+    likelihoods = []
     for term_freq in self.term_freq_by_class:
-      freq = np.prod(np.vectorize(term_freq.get)(x) / self.VOCABULARY_SIZE)
-      fr_by_cl.append(freq)
+      probs = np.vectorize(self.smoothed(term_freq))(tokens) / self.VOCABULARY_SIZE
+      prob = math.exp(np.sum(np.log(probs)))
+      likelihoods.append(prob)
 
-    return np.array(fr_by_cl)
+    return np.array(likelihoods)
+
+  def prepare_for_laplace_smoothing(self, term_freq):
+      for token in self.term_freq.keys():
+        if token not in term_freq: term_freq[token] = 0
+
+  # Laplace smoothing
+  def smoothed(self, term_freq):
+    # We believe, that prior is made up in such a way, as if we had observed every feature
+    # at least once. And now we are updating the prior with seen data.
+    return lambda token: term_freq.get(token) + 1
+
+  # prob_x
+  def evidence(self, x):
+    # We do not adjust prior by P(X) since it is the same for all classes.
+    # You may say that it would affect the final propability we get, but
+    # there is no concern here because probability in Bayes Naive is
+    # essentially undermined, garbled and usually very close to 0 or 1.
+    return 1
 
   def tokenize(self, text):
     words = self.cleaner.words(text)
@@ -76,10 +92,7 @@ class NBClassifier(object):
     return words
 
   def count_words(self, words):
-    count = [['UNK', -1]]
-    count.extend(collections.Counter(words).most_common(self.VOCABULARY_SIZE - 1))
-
-    return count
+    return collections.Counter(words).most_common(self.VOCABULARY_SIZE)
 
   def build_term_frequencies(self, count):
     assert self.dictionary, 'Cannot count words - a dictionary should be build first.'
