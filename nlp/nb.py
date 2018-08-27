e9d0c12de0c5f52c528f3e24140b9e070af90471
Oleg <gafrom@gmail.com>
2018-08-27 22:27:29 +0300

    Fix TF probability calculus and add TFIDF one

--- nlp/nb.py
+++ nlp/nb.py
@@ -2,37 +2,42 @@ import collections
 from nlp.cleaner import Cleaner
 import numpy as np
 import math
+import functools as fn
 
 class NBClassifier(object):
   VOCABULARY_SIZE = 50000
   CORRELATION_LIMIT = 0.1
+  TF_STRATEGY    = 'tf'
+  TFIDF_STRATEGY = 'tfidf'
 
-  def __init__(self):
-    self.cleaner = Cleaner()
-
-    self.dictionary = None
-
-    self.prob_Ck            = None
-    self.term_freq          = None
-    self.term_freq_by_class = None
-
+  def __init__(self, strategy = TFIDF_STRATEGY, cleaner = Cleaner()):
+    self.strategy = strategy
+    self.cleaner  = cleaner
+    self.dictionary  = None
+    self.prob_Ck     = None
+    self.tf          = None
+    self.tf_by_class = None
+    self.idf         = None
     self.result_class = collections.namedtuple('NaiveBayesClassifierResult', ('label', 'probs'))
 
   def train(self, labeled_documents):
     self.build_dictionary(labeled_documents)
-    term_freq_by_class = []
+    tf_by_class = []
 
     for _, documents in labeled_documents.groupby('label'):
       count = self.count_words(self.extract_words(documents))
-      term_freq = self.build_term_frequencies(count)
-      self.prepare_for_laplace_smoothing(term_freq)
-      term_freq_by_class.append(term_freq)
+      tf = self.build_term_frequencies(count)
+      self.prepare_for_laplace_smoothing(tf)
+      tf_by_class.append(tf)
 
-    self.term_freq_by_class = np.array(term_freq_by_class)
-    self.prob_Ck = 1 / len(self.term_freq_by_class)
+    self.tf_by_class = np.array(tf_by_class)
+    self.prob_Ck = 1 / len(self.tf_by_class)
 
     self.remove_highly_correlated_features()
     self.remove_stop_words()
+
+    if self.strategy == self.TFIDF_STRATEGY: self.compute_idf(labeled_documents)
+    self.compute_prob_denominators()
 
   def predict(self, text):
     assert self.prob_Ck, 'Cannot infer - model should be trained first.'
@@ -49,27 +54,33 @@ class NBClassifier(object):
   # prob_x_given_Ck
   def likelihood(self, tokens):
     likelihoods = []
-    for term_freq in self.term_freq_by_class:
-      probs = np.vectorize(self.smoothed(term_freq))(tokens) / self.VOCABULARY_SIZE
-      prob = math.exp(np.sum(np.log(probs)))
-      likelihoods.append(prob)
+    for class_index, tf in enumerate(self.tf_by_class):
+      prob_numerators  = np.vectorize(self.smoothed(tf))(tokens)
+      prob_denominator = self.prob_denominators[self.strategy][class_index]
+      probs = prob_numerators / prob_denominator
+
+      probability = math.exp(np.sum(np.log(probs)))
+      likelihoods.append(probability)
 
     return np.array(likelihoods)
 
   def prepare_for_laplace_smoothing(self, term_freq):
-      for token in self.term_freq.keys():
-        if token not in term_freq: term_freq[token] = 0
+    for token in self.tf.keys():
+      if token not in term_freq: term_freq[token] = 0
 
   # Laplace smoothing
-  def smoothed(self, term_freq):
+  def smoothed(self, tf):
     # We believe, that prior is made up in such a way, as if we had observed every feature
     # at least once. And now we are updating the prior with seen data.
-    return lambda token: term_freq.get(token) + 1
+    if self.strategy == self.TFIDF_STRATEGY:
+      return lambda token: tf[token] * self.idf[token] + 1
+    else:
+      return lambda token: tf[token] + 1
 
   # prob_x
   def evidence(self, x):
     # We do not adjust prior by P(X) since it is the same for all classes.
-    # You may say that it would affect the final propability we get, but
+    # You may say that it would affect the final probability we get, but
     # there is no concern here because probability in Bayes Naive is
     # essentially undermined, garbled and usually very close to 0 or 1.
     return 1
@@ -85,7 +96,7 @@ class NBClassifier(object):
     self.dictionary = dict()
     for word, _ in count: self.dictionary[word] = len(self.dictionary)
     self.reverse_dictionary = dict(zip(self.dictionary.values(), self.dictionary.keys()))
-    self.term_freq = self.build_term_frequencies(count)
+    self.tf = self.build_term_frequencies(count)
 
   def extract_words(self, documents):
     words = []
@@ -105,11 +116,11 @@ class NBClassifier(object):
   def remove_highly_correlated_features(self):
     to_be_removed = []
 
-    for token in self.term_freq.keys():
-      freqs = [freq[token] for freq in self.term_freq_by_class]
+    for token in self.tf.keys():
+      freqs = [freq[token] for freq in self.tf_by_class]
 
       # TODO: extrapolate for multinomial classifier
-      if abs(freqs[0] - freqs[1]) / self.term_freq[token] < self.CORRELATION_LIMIT:
+      if abs(freqs[0] - freqs[1]) / self.tf[token] < self.CORRELATION_LIMIT:
         to_be_removed.append((token, freqs))
 
     for token, freqs in to_be_removed:
@@ -117,19 +128,49 @@ class NBClassifier(object):
       print(f"Removing word {word} for {freqs}")
       self.dictionary.pop(word, None)
       self.reverse_dictionary.pop(token, None)
-      self.term_freq.pop(token, None)
-      for term_freq in self.term_freq_by_class:
-        term_freq.pop(token, None)
+      self.tf.pop(token, None)
+      for tf in self.tf_by_class:
+        tf.pop(token, None)
 
 
   def remove_stop_words(self):
-    to_be_removed = [token for token in self.term_freq.keys() if self.term_freq[token] > 5000]
+    to_be_removed = [token for token in self.tf.keys() if self.tf[token] > 5000]
 
     for token in to_be_removed:
       word = self.reverse_dictionary[token]
       print(f"Removing stop word {word}")
       self.dictionary.pop(word, None)
       self.reverse_dictionary.pop(token, None)
-      self.term_freq.pop(token, None)
-      for term_freq in self.term_freq_by_class:
-        term_freq.pop(token, None)
+      self.tf.pop(token, None)
+      for tf in self.tf_by_class:
+        tf.pop(token, None)
+
+  def compute_idf(self, labeled_documents):
+    df = collections.defaultdict(int)
+
+    tokenized_documents = []
+    for document in labeled_documents['body']:
+      doc = { self.dictionary[word] for word in self.cleaner.words(document) if word in self.dictionary }
+      tokenized_documents.append(doc)
+
+    print(f"Tokenized {len(tokenized_documents)} documents")
+    for key in self.reverse_dictionary.keys():
+      for document in tokenized_documents:
+        if key in document: df[key] += 1
+
+    self.idf = { key: math.log(len(labeled_documents) / df[key]) for key in df.keys() }
+    print('Built IDF')
+
+  def compute_prob_denominators(self):
+    # + 1 and len(tf) are needed to account for Laplace smoothing
+    tfidf_den = []
+
+    if self.strategy == self.TFIDF_STRATEGY:
+      for tf in self.tf_by_class:
+        den = fn.reduce((lambda total, key: total + (tf[key] + 1) * self.idf[key]), tf.keys())
+        tfidf_den.append(den)
+
+    self.prob_denominators = {
+      'tf'    : [(sum(tf.values()) + len(tf)) for tf in self.tf_by_class],
+      'tfidf' : tfidf_den
+    }
