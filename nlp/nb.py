299aa406010757d22516417976c9a6af6a4ab8f4
Oleg <gafrom@gmail.com>
2018-09-05 23:49:18 +0300

    Use Term Frequency adjusted for document length

--- nlp/nb.py
+++ nlp/nb.py
@@ -18,6 +18,7 @@ class NBClassifier(object):
     self.tf          = None
     self.tf_by_class = None
     self.idf         = None
+    self.once        = None
     self.result_class = collections.namedtuple('NaiveBayesClassifierResult', ('label', 'probs'))
 
   def train(self, labeled_documents):
@@ -25,9 +26,7 @@ class NBClassifier(object):
     tf_by_class = []
 
     for _, documents in labeled_documents.groupby('label'):
-      count = self.count_words(self.extract_words(documents))
-      tf = self.build_term_frequencies(count)
-      self.prepare_for_laplace_smoothing(tf)
+      tf = self.compute_tf(self.documents_as_words(documents))
       tf_by_class.append(tf)
 
     self.tf_by_class = np.array(tf_by_class)
@@ -66,18 +65,15 @@ class NBClassifier(object):
 
     return np.array(log_likelihoods)
 
-  def prepare_for_laplace_smoothing(self, term_freq):
-    for token in self.tf.keys():
-      if token not in term_freq: term_freq[token] = 0
-
   # Laplace smoothing
   def smoothed(self, tf):
     # We believe, that prior is made up in such a way, as if we had observed every feature
     # at least once. And now we are updating the prior with seen data.
+
     if self.strategy == self.TFIDF_STRATEGY:
-      return lambda token: tf[token] * self.idf[token] + 1
+      return lambda token: (tf[token] + self.once) * self.idf[token]
     else:
-      return lambda token: tf[token] + 1
+      return lambda token:  tf[token] + self.once
 
   # prob_x
   def evidence(self, x):
@@ -93,27 +89,29 @@ class NBClassifier(object):
     return [self.dictionary[word] for word in words if word in self.dictionary]
 
   def build_dictionary(self, documents):
-    words = self.extract_words(documents)
-    count = self.count_words(words)
+    documents_as_words = self.documents_as_words(documents)
+    words = [word for words in documents_as_words for word in words]
+    count = collections.Counter(words).most_common(self.VOCABULARY_SIZE)
     self.dictionary = dict()
     for word, _ in count: self.dictionary[word] = len(self.dictionary)
     self.reverse_dictionary = dict(zip(self.dictionary.values(), self.dictionary.keys()))
-    self.tf = self.build_term_frequencies(count)
+    self.tf = self.compute_tf(documents_as_words)
+    self.once = 1 / (sum(len(x) for x in documents_as_words) / len(documents))
 
-  def extract_words(self, documents):
-    words = []
+  def documents_as_words(self, documents):
+    return [self.cleaner.words(document) for document in documents['body']]
 
-    for document in documents['body']:
-      words.extend(self.cleaner.words(document))
+  # here we use advanced definition of Term Frequence:
+  # raw count of a term in a document adjusted for document length
+  def compute_tf(self, documents):
+    tf = collections.defaultdict(int)
 
-    return words
+    for words in documents:
+      for word in (x for x in words if x in self.dictionary):
+        # tf[self.dictionary[word]] += 1
+        tf[self.dictionary[word]] += 1 / len(words)
 
-  def count_words(self, words):
-    return collections.Counter(words).most_common(self.VOCABULARY_SIZE)
-
-  def build_term_frequencies(self, count):
-    assert self.dictionary, 'Cannot count words - a dictionary should be build first.'
-    return { self.dictionary[key]:value for key, value in count if key in self.dictionary }
+    return tf
 
   def remove_highly_correlated_features(self):
     to_be_removed = []
@@ -133,7 +131,6 @@ class NBClassifier(object):
       self.tf.pop(token, None)
       for tf in self.tf_by_class:
         tf.pop(token, None)
-
 
   def remove_stop_words(self):
     to_be_removed = [token for token in self.tf.keys() if self.tf[token] > 5000]
@@ -164,15 +161,15 @@ class NBClassifier(object):
     print('Built IDF')
 
   def compute_prob_denominators(self):
-    # + 1 and len(tf) are needed to account for Laplace smoothing
+    # `self.once` are needed to account for Laplace smoothing
     tfidf_den = []
 
     if self.strategy == self.TFIDF_STRATEGY:
       for tf in self.tf_by_class:
-        den = fn.reduce((lambda total, key: total + (tf[key] + 1) * self.idf[key]), tf.keys())
+        den = fn.reduce((lambda total, key: total + (tf[key] + self.once) * self.idf[key]), tf.keys())
         tfidf_den.append(den)
 
     self.prob_denominators = {
-      'tf'    : [(sum(tf.values()) + len(tf)) for tf in self.tf_by_class],
+      'tf'    : [(sum(tf.values()) + len(tf) * self.once) for tf in self.tf_by_class],
       'tfidf' : tfidf_den
     }
